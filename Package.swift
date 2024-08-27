683fe47fdf820b961361a51a3971048862f86cc4
Edon <edon@valdman.works>
2024-08-27 15:44:13 -0500

    Added dependencies for OpenAPI generating

--- Package.swift
+++ Package.swift
@@ -5,12 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "self-server-openapi-swift",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SelfServerRESTTypes",
             targets: ["SelfServerRESTTypes"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
