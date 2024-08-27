e57c5bab886affeaac844b360537c65d0585cc7c
Edon <edon@valdman.works>
2024-08-27 15:05:00 -0500

    Renamed first target/library

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,16 +8,19 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "self-server-openapi-swift",
-            targets: ["self-server-openapi-swift"]),
+            name: "SelfServerRESTTypes",
+            targets: ["SelfServerRESTTypes"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "self-server-openapi-swift"),
+            name: "SelfServerRESTTypes"
+        ),
         .testTarget(
             name: "self-server-openapi-swiftTests",
-            dependencies: ["self-server-openapi-swift"]),
+            dependencies: ["self-server-openapi-swift"]
+        ),
     ]
 )
