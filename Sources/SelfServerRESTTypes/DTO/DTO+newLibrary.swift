e8935b46bc44ce955afb1fe78bcf6db7dc9e7e2d
Edon <edon@valdman.works>
2024-09-08 17:53:56 -0500

    Create DTO+newLibrary.swift
    New DTO type.

---
+++ Sources/SelfServerRESTTypes/DTO/DTO+newLibrary.swift
@@ -0,0 +1,18 @@
+//
+//  DTO+newLibrary.swift
+//
+//
+//  Created by Edon Valdman on 9/8/24.
+//
+
+import Foundation
+
+extension SelfServerDTOs {
+    /// Client-side DTO for `newLibrary` operation.
+    ///
+    /// Used by `SelfServerRESTClient` as operation input.
+    public struct NewLibrary: Sendable {
+        public let name: String
+        public let deviceID: UUID
+    }
+}
