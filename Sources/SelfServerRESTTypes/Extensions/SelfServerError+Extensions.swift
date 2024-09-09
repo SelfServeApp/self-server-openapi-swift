5dc2c6b03e6f0bf7d56fe093c19582b6a87baf53
Edon <edon@valdman.works>
2024-09-09 11:31:30 -0500

    Update SelfServerError+Extensions.swift
    Added init for new `UnknownCodeError`.

--- Sources/SelfServerRESTTypes/Extensions/SelfServerError+Extensions.swift
+++ Sources/SelfServerRESTTypes/Extensions/SelfServerError+Extensions.swift
@@ -8,6 +8,8 @@
 import Foundation
 import SelfServerHelperTypes
 
+import NIOHTTP1
+
 extension SelfServerError {
     /// A convenience initializer from a `GeneralError` response, which contains an `X-Self-Server-Error-Code` header.
     ///
@@ -35,5 +37,19 @@ extension UnauthorizedRequestError {
             operationID: operationID,
             wwwAuthenticate: response.headers.WWW_hyphen_Authenticate
         )
+    }
+}
+
+extension UnknownCodeError {
+    /// A convenience initializer from a `GeneralError` response, used when the content of the `X-Self-Server-Error-Code` header is not a recognized value.
+    public init(status: HTTPResponseStatus, response: Components.Responses.GeneralError) {
+        switch response.body {
+        case .json(let body):
+            self.init(
+                statusCode: status,
+                errorCode: response.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code,
+                reason: body.reason
+            )
+        }
     }
 }
