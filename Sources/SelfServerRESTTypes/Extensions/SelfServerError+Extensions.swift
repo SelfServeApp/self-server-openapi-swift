7e1c7e73dfb43d51054a69c9528036f2a4d44533
Edon <edon@valdman.works>
2024-09-16 11:09:31 -0500

    Update SelfServerError+Extensions.swift
    Removed code relating to now-deleted `403ForbiddenResponse`.

--- Sources/SelfServerRESTTypes/Extensions/SelfServerError+Extensions.swift
+++ Sources/SelfServerRESTTypes/Extensions/SelfServerError+Extensions.swift
@@ -19,15 +19,6 @@ extension SelfServerError {
         guard let error = SelfServerError(rawValue: UInt(errorCodeInt)) else { return nil }
         self = error
     }
-    
-    /// A convenience initializer from a `403`/`Forbidden` response, which contains an `X-Self-Server-Error-Code` header.
-    ///
-    /// It might fail if it's not a valid error code.
-    public init?(response: Components.Responses._403ForbiddenResponse) {
-        let errorCodeInt = response.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code
-        guard let error = SelfServerError(rawValue: UInt(errorCodeInt)) else { return nil }
-        self = error
-    }
 }
 
 extension UnauthorizedRequestError {
@@ -43,18 +34,6 @@ extension UnauthorizedRequestError {
 extension UnknownCodeError {
     /// A convenience initializer from a `GeneralError` response, used when the content of the `X-Self-Server-Error-Code` header is not a recognized value.
     public init(status: HTTPResponseStatus, response: Components.Responses.GeneralError) {
-        switch response.body {
-        case .json(let body):
-            self.init(
-                statusCode: status,
-                errorCode: response.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code,
-                reason: body.reason
-            )
-        }
-    }
-    
-    /// A convenience initializer from a `403`/`Forbidden` response, used when the content of the `X-Self-Server-Error-Code` header is not a recognized value.
-    public init(status: HTTPResponseStatus, response: Components.Responses._403ForbiddenResponse) {
         switch response.body {
         case .json(let body):
             self.init(
