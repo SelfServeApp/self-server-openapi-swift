26cbcb0b2331b183cc5954384409615cb2a3e370
Edon <edon@valdman.works>
2024-09-16 11:27:13 -0500

    Update DTO+newLibrary.swift
    Added a `ConflictError` type.

--- Sources/SelfServerRESTTypes/DTO/DTO+newLibrary.swift
+++ Sources/SelfServerRESTTypes/DTO/DTO+newLibrary.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SelfServerHelperTypes
 
 extension SelfServerDTOs {
     /// Client-side DTO for `newLibrary` operation.
@@ -18,6 +19,25 @@ extension SelfServerDTOs {
         public init(name: String, deviceID: UUID) {
             self.name = name
             self.deviceID = deviceID
+        }
+        
+        public struct ConflictError: LocalizedError, Sendable {
+            public typealias Body = Components.Responses._409ConflictingLibraryResponse.Body.jsonPayload
+            
+            public let body: Body
+            public let errorCode: SelfServerError
+            
+            internal init(response: Components.Responses._409ConflictingLibraryResponse) {
+                self.body = switch response.body {
+                case .json(let body): body
+                }
+                
+                self.errorCode = .init(errorCode: response.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code)
+                    ?? .newLibraryDeviceIDAlreadyExists
+            }
+            
+            public var errorDescription: String? { errorCode.errorDescription }
+            public var failureReason: String? { errorCode.failureReason }
         }
     }
 }
