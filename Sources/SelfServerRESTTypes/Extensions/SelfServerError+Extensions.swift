0470521a65a9951ca9a5131f201781c1c0ad2651
Edon <edon@valdman.works>
2024-09-16 11:22:49 -0500

    Update SelfServerError+Extensions.swift
    Split initializer into 2, one that takes a full `GeneralError` response, and one that takes just the error code.

--- Sources/SelfServerRESTTypes/Extensions/SelfServerError+Extensions.swift
+++ Sources/SelfServerRESTTypes/Extensions/SelfServerError+Extensions.swift
@@ -15,9 +15,14 @@ extension SelfServerError {
     ///
     /// It might fail if it's not a valid error code.
     public init?(response: Components.Responses.GeneralError) {
-        let errorCodeInt = response.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code
-        guard let error = SelfServerError(rawValue: UInt(errorCodeInt)) else { return nil }
-        self = error
+        self.init(errorCode: response.headers.X_hyphen_Self_hyphen_Server_hyphen_Error_hyphen_Code)
+    }
+    
+    /// A convenience initializer from a `GeneralError` response, which contains an `X-Self-Server-Error-Code` header.
+    ///
+    /// It might fail if it's not a valid error code.
+    public init?(errorCode header: Components.Headers.XSelfServerErrorCode) {
+        self.init(rawValue: UInt(header))
     }
 }
 
